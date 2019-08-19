ed7e17a535aabbb1b253ef37288dc10e460cd617
Fscibe <fl.sciberras@gmail.com>
2019-08-19 13:45:30 +0200

    Add unity project.

---
+++ Assets/PackageBuilder/Editor/PackageBuilder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class PackageBuilder
+{
+	[MenuItem("PackageBuilder/Export package")]
+	public static void BuildPackage()
+	{
+		const string PACKAGE_NAME = "ScreenshotTool";
+
+		var folders = new string[]{
+			"Assets/" + PACKAGE_NAME
+		};
+
+		var guids = AssetDatabase.FindAssets("", folders);
+		var assets = new string[guids.Length];
+		for (int i = 0; i < guids.Length; ++i)
+		{
+			assets[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+		}
+
+		string exportPath = Application.dataPath;
+		var file = EditorUtility.SaveFilePanel("Export Package", exportPath, PACKAGE_NAME, "unitypackage");
+		if (!string.IsNullOrEmpty(file))
+		{
+			AssetDatabase.ExportPackage(assets, file, ExportPackageOptions.Default);
+		}
+	}
+}
---
+++ Assets/ScreenshotTool/Editor/ScreenshotTool.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Take screenshots of the Game window, at selected resolution.
+/// </summary>
+public class ScreenshotToolWindow : EditorWindow
+{
+	private const string SAVE_KEY_FILENAME = "ScreenshotToolWindow_FileName";
+
+	private string	_filename = "";
+	private bool	_addResolutionSuffix = true;
+	private string	_lastSavedFilename;
+	private string	_folderName = "screenshots";
+	private string	_outputDirectoryPath = "";
+
+	[MenuItem("Tools/Screenshot")]
+	static void Init()
+	{
+		ScreenshotToolWindow instance = GetWindow(typeof(ScreenshotToolWindow), false, "Screenshot") as ScreenshotToolWindow;
+		instance._filename = EditorPrefs.GetString(SAVE_KEY_FILENAME, "screenshot");
+	}
+
+	private void CreateOutputDirectory()
+	{
+		if (!Directory.Exists(_outputDirectoryPath))
+		{
+			Directory.CreateDirectory(_outputDirectoryPath);
+		}
+	}
+
+	private void OnGUI()
+	{
+		EditorGUILayout.BeginVertical();
+		{
+			_outputDirectoryPath = Application.persistentDataPath + "/" + _folderName;
+
+			// screenshot name
+			EditorGUILayout.BeginHorizontal();
+			{
+				EditorGUILayout.LabelField("Screenshot name", GUILayout.Width(128f));
+
+				EditorGUI.BeginChangeCheck();
+				_filename = GUILayout.TextField(_filename);
+				if (EditorGUI.EndChangeCheck())
+				{
+					EditorPrefs.SetString(SAVE_KEY_FILENAME, _filename);
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+
+			_addResolutionSuffix = GUILayout.Toggle(_addResolutionSuffix, "Add screen resolution to base name");
+
+			// capture
+			if (GUILayout.Button("Take screenshot!"))
+			{
+				// resolution
+				string suffix = "";
+				if (_addResolutionSuffix)
+				{
+					suffix = "_" + Camera.main.pixelWidth + "x" + Camera.main.pixelHeight;
+				}
+
+				// number
+				int number = -1;
+				string fullFileName;
+				string fullFilePath;
+				do
+				{
+					++number;
+					fullFileName = _filename + suffix + "_" + number.ToString("000") + ".png";
+					fullFilePath = _outputDirectoryPath + "/" + fullFileName;
+					_lastSavedFilename = fullFileName;
+				}
+				while (File.Exists(fullFilePath) && number <= 999);
+
+				// screenshot
+				CreateOutputDirectory();
+				ScreenCapture.CaptureScreenshot(fullFilePath);
+			}
+
+			// destination folder
+			if (GUILayout.Button("Show destination folder"))
+			{
+				CreateOutputDirectory();
+				EditorUtility.RevealInFinder(_outputDirectoryPath);
+			}
+
+			// capture feedback
+			if (!string.IsNullOrEmpty(_lastSavedFilename))
+			{
+				EditorGUILayout.LabelField("Saved as '" + _lastSavedFilename + "'!");
+			}
+		}
+		EditorGUILayout.EndVertical();
+	}
+}
